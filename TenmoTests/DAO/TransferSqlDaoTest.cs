b63e3200509c983277c6fea573525f249de11b7a
Tommy Hayden <tommyhayden78@gmail.com>
2024-03-08 15:58:57 -0500

    working on transfer test

--- TenmoTests/DAO/TransferSqlDaoTest.cs
+++ TenmoTests/DAO/TransferSqlDaoTest.cs
@@ -14,8 +14,8 @@ namespace TenmoTests.Tests.DAO
     {
         /*  (1001, 1000.00), -- id will be 2001
 	   (1002, 1000.00); -- id will be 2002 */
-        private static readonly Account ACCOUNT_1 = new Account(2001, 1001, 1000.00M);
-        private static readonly Account ACCOUNT_2 = new Account(2002, 1002, 1000.00M);
+        private static readonly Transfer TRANSFER_1TransferOut= new Transfer(9999,2,2,2001,2002,50m);
+        private static readonly Transfer TRANSFER_2TransferRequest= new Transfer(0, 1, 1, 2002, 2002, 50m);
 
         private TransferSqlDao dao;
 
@@ -27,6 +27,24 @@ namespace TenmoTests.Tests.DAO
             dao = new TransferSqlDao(ConnectionString);
             testTransfer = new Transfer();
             base.Setup();
+        }
+        [TestMethod]
+        public void CreateTransferCreatesATransfer()
+        {
+            dao.CreateTransfer(TRANSFER_1TransferOut);
+            Transfer actual = dao.GetTransferById(9999);
+            TransfersAreEqaul(TRANSFER_1TransferOut, actual);
+            
+        }
+        public void TransfersAreEqaul(Transfer t1, Transfer t2)
+        {
+            Assert.AreEqual(t1.AccountFrom, t2.AccountFrom);
+            Assert.AreEqual(t1.AccountTo, t2.AccountTo);
+            Assert.AreEqual(t1.Amount, t2.Amount);
+            Assert.AreEqual(t1.TransferId, t2.TransferId);
+            Assert.AreEqual(t1.TransferStatusId, t2.TransferStatusId);
+            Assert.AreEqual(t1.TransferTypeId, t2.TransferTypeId);
+
         }
     }
 
